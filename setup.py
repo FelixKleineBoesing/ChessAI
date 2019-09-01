c2a90d44571582647f6ea7eb282e5c041a2e9a52
Felix Kleine Bösing <kleine-boesing@westphalia-datalab.com>
2019-09-01 16:35:58 +0200

    renamed checkers to chess, created dir stones where the different stone classes will be implemented

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-setup(name='checkers',
-      version='0.1',
+setup(name='chess',
+      version='0.1-beta',
       description='Checkers environment and Reinforcement Learning',
       url='',
       author='Felix Kleine Bösing',
       author_email='felix.boesing@t-online.de',
       license='MIT',
-      packages=["checkers"],
-      install_requires=['numpy', "pandas", 'tensorflow==v2.0.0-beta0', 'keras', "simplejson", "redis", "aioredis"],
+      packages=["chess"],
+      install_requires=['numpy', "pandas", 'tensorflow==v2.0.0-beta1', 'keras', "simplejson", "redis", "aioredis"],
       zip_safe=False)
